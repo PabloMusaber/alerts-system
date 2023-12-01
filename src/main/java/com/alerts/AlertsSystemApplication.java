ee60eee2fd65d18c42b9d1fedbd3e3fe51e8b1fa
Pablo <musaberpablo@gmail.com>
2023-11-30 22:39:47 -0300

    Initial commit

---
+++ src/main/java/com/alerts/AlertsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertsSystemApplication.class, args);
+	}
+
+}
